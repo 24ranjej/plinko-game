5d7faf3e50bbd69001bb7b19f3eac3451a8daa9a
24ranjej <69919771+24ranjej@users.noreply.github.com>
2020-11-21 21:34:39 -0600

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,20 @@
+class Plinko{
+    constructor(x,y,width,height,angle){
+        var options={
+            isStatic: false
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.heigght = height;
+        World.add(world,this.body);
+    }
+    display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    ellipse(0,0,this.width,this.height);
+    pop();
+    }
+}
